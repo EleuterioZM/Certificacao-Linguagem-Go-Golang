3c7ef30a1834e1a3d0c8e77de348db7298d8238d
Eleuterio Zacarias Mabecuane <122021325+EleuterioZM@users.noreply.github.com>
2025-02-04 23:29:46 +0200

    #Feature: Fluco de controle

---
+++ Fluxo de Controle/ifelseif.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+
+	var a int = 11
+
+	if a > 10 {
+		println("A e maior que 10, o valor de a e", a)
+
+	}else if  a >5{
+		println("A esta entre 6 e 10, o valor de a e", a)
+	}else{
+		println("A e menor ou igual a 5, o valor de a e", a)
+	}
+
+}
