0e08719eaf73375885f07815c8924bc70348eb1f
Eleuterio Zacarias Mabecuane <122021325+EleuterioZM@users.noreply.github.com>
2025-02-04 15:24:10 +0200

    #Feature: Operacdores de Comparacao

---
+++ Operacdores de Comparacao/comparacao.go
@@ -0,0 +1,19 @@
+package main
+
+
+
+
+func main() {
+
+	//> < >= <= == !=
+var a int = 22
+var b int = 7
+
+println("A > B :", a > b)
+println("A < B :", a < b)
+println("A >= B :", a >= b)
+println("A <= B :", a <= b)
+println("A == B :", a == b)
+println("A != B :", a != b)
+
+}
